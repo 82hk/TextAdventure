72a04adf16f7d0e316f651b8973594c9085314ba
connertm24 <connertm24@sherwoodsd.com>
2023-02-03 11:59:33 -0800

    Game->game

--- src/main/java/org/sherwoodhs/Main.java
+++ src/main/java/org/sherwoodhs/Main.java
@@ -7,7 +7,7 @@ import org.sherwoodhs.situation.Situation;
 
 public class Main {
     public static void main(String[] args) {
-        AdvGame ikeaGame = AdvGame.getInstance();
+        AdvGame game = AdvGame.getInstance();
         Location currentLocation = new Entrance();
         Situation currentSituation = currentLocation.getStartSituation();
         currentSituation.performAction(0);
