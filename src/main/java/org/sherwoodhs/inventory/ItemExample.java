63dd094865dabf53cd47199179d07a3a4f8999cc
Chris P <christopherip23@sherwoodsd.com>
2023-02-09 10:49:31 -0800

    a

--- src/main/java/org/sherwoodhs/inventory/ItemExample.java
+++ src/main/java/org/sherwoodhs/inventory/ItemExample.java
@@ -3,7 +3,7 @@ package org.sherwoodhs.inventory;
 public class ItemExample extends Item{
     public ItemExample() {
         super("Box",
-                "An simple cardboard box.",
+                "A simple cardboard box.",
                 "Upon further inspection, you find the box is damaged, and inside is a plastic ruler.");
     }
 }
