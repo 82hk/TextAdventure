b62e76a98bf04182628c1e5ad7b26aba8f1bae94
Archnitest <derekxl24@sherwoodsd.com>
2023-02-14 12:59:41 -0800

    Little changes

--- src/main/java/org/sherwoodhs/situation/OtherSide1/OtherSide1_0_0D.java
+++ src/main/java/org/sherwoodhs/situation/OtherSide1/OtherSide1_0_0D.java
@@ -1,5 +1,6 @@
 package org.sherwoodhs.situation.OtherSide1;
 
+import org.sherwoodhs.AdvGame;
 import org.sherwoodhs.player.Player;
 import org.sherwoodhs.npc.Alchemist;
 import org.sherwoodhs.npc.NPC;
@@ -7,18 +8,19 @@ import org.sherwoodhs.situation.Situation;
 /*
     This is where you first meet the mysterious Alchemist(At this point, known as ?????).
     This only happens on your first entrance of the Hidden Alcove.
-    Really more like an introduction to questline before it is initiated. 
+    Really more like an introduction to questline before it is initiated.
+    Leads to some options like the questline never starting or some early relationship boost.
 */
-public class OtherSide1_0_0E implements Situation {
+public class OtherSide1_0_0D implements Situation {
     private Player player = Player.getInstance();
     private NPC alchemist = Alchemist.getInstance();
 
     // desc
-    private static Situation exploration = new OtherSide1_0_0E();
-    private OtherSide1_0_0E() {}
+    private static Situation exploration = new OtherSide1_0_0D();
+    private OtherSide1_0_0D() {}
 
     @Override
-    public String getSitType() {return "Exploration";}
+    public String getSitType() {return "Dialogue";}
 
     @Override
     public String getTitle() {return "An Unexpected Meeting";}
@@ -31,25 +33,33 @@ public class OtherSide1_0_0E implements Situation {
                 "They are cloaked in a dark garment. In the dim light, you can't distinguish what specific color it is.\n\n" +
                 "You paused for a second before answering slowly.\n\n" +
                 player.getName() + ": Calm down. I come in peace. An Old Man told me about this place. Said I could find ___________.\n\n" +
-                "The figure noticably relaxes and sighs." +
+                "The figure noticeably relaxes and sighs." +
                 alchemist.getName() + ": I don't what my grandpa said to you, but he's wrong. You'll find nothing here but forsaken dreams " +
                 "and lost happiness. Let me give you a piece of advice. Leave this place. Get involved with one of those factions out there. " +
                 "Make friends and enemies... Win the loot of victors... Maybe find true love... I don't know what you want. " +
                 "Just don't waste your time here. Lose yourself in the stimulations of this cursed place. " +
-                "The anarchy giving us the percieved freedom of choice is the only thing it's good for anyway. " +
+                "The anarchy giving us the perceived freedom of choice is the only thing it's good for anyway. " +
                 "Too bad seeing behind the curtain ruins everything...\n\n" +
                 "Silence befalls you two. It permeates the room, until you say...");
     }
 
     @Override
     public String[] getOptions() {
-        String[] options = { "\"\""};
+        String[] options = { "\"What curtain?\"", "\"I'll take your advice and leave\"", "", ""};
+        if ( true /*Some kind of really-bad reputation with everyone*/){
+            options[3] = "\"There's nothing for me to return to.\"";
+
+        }
         return options;
     }
 
     @Override
     public void perform(String option) {
         switch (option){
+            case "\"There's nothing for me to return to.\"":
+                AdvGame.getInstance().setSituation(OtherSide1_1_0D.getInstance());
+                alchemist.changeRelModifier(5);
+            break;
 
         }
     }
