a6d14f064ac3f4d9031c7014c3e1e6c5f6644b9c
GenericProgram <gddiamondnoob@gmail.com>
2023-02-16 13:00:25 -0800

    fix

--- src/main/java/org/sherwoodhs/AdvGame.java
+++ src/main/java/org/sherwoodhs/AdvGame.java
@@ -1,7 +1,7 @@
 package org.sherwoodhs;
 import org.sherwoodhs.situation.Situation;
-import org.sherwoodhs.situation.Testing.TestConversation1.TestConversation1_0D;
-import org.sherwoodhs.situation.examples.ExampleSituation;
+import org.sherwoodhs.situation.entrance.EntranceSituation_0E;
+
 
 import javax.swing.border.TitledBorder;
 import java.awt.*;
