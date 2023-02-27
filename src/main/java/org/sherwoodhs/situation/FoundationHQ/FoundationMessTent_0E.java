cff7fca124636fca08c18b3084f001c9175942ed
GenericProgram <gddiamondnoob@gmail.com>
2023-02-27 13:00:06 -0800

    progress

---
+++ src/main/java/org/sherwoodhs/situation/FoundationHQ/FoundationMessTent_0E.java
@@ -0,0 +1,51 @@
+package org.sherwoodhs.situation.FoundationHQ;
+
+import org.sherwoodhs.AdvGame;
+import org.sherwoodhs.situation.Haven.HavenCenter_E;
+import org.sherwoodhs.situation.SitType;
+import org.sherwoodhs.situation.Situation;
+
+public class FoundationMessTent_0E implements Situation {
+    private static Situation situation = new FoundationMessTent_0E();
+
+    @Override
+    public String getTitle() {
+        return "The Foundation HQ";
+    }
+
+    @Override
+    public String getDescription() {
+        return "do";
+    }
+
+    @Override
+    public SitType getSitType() {
+        return SitType.Hub;
+    }
+
+    @Override
+    public String[] getOptions() {
+        String[] options = {"Go to armory tent", "Go to command tent", "Go to mess tent", "Go back to The Haven"};
+        return options;
+    }
+
+    @Override
+    public void perform(String option) {
+        switch(option) {
+            case "Go to armory tent":
+
+                break;
+            case "Go back to The Haven":
+                AdvGame.setSituation(HavenCenter_E.getInstance());
+                break;
+            case "Go to command tent":
+                break;
+            case "Go to mess tent":
+                break;
+        }
+    }
+
+    public static Situation getInstance(){
+        return situation;
+    }
+}
