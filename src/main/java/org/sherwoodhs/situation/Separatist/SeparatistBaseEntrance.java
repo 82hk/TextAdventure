a4a74e3fb0f0daba50168334ad89d76f61b129b5
Tyler <tylerkn23@sherwoodsd.com>
2023-03-03 12:07:23 -0800

    separatist story stuff

--- src/main/java/org/sherwoodhs/situation/Separatist/SeparatistBaseEntrance.java
+++ src/main/java/org/sherwoodhs/situation/Separatist/SeparatistBaseEntrance.java
@@ -3,12 +3,16 @@ package org.sherwoodhs.situation.Separatist;
 import org.sherwoodhs.AdvGame;
 import org.sherwoodhs.situation.SitType;
 import org.sherwoodhs.situation.Situation;
+import org.sherwoodhs.npc.Separatist.SeparatistChief;
+import org.sherwoodhs.npc.NPC;
 
 public class SeparatistBaseEntrance implements Situation {
     
     // REVERT EVERYTHING TO EXPLORATION TYPE, MAKE INDIVIDUAL CLASSES FOR EVERY OPTION (maybe with adding previous text in backwards).
     private static boolean firstTime = true;
     private static Situation situation = new SeparatistBaseEntrance();
+
+    NPC chief = SeparatistChief.getInstance();
 
     @Override
     public String getTitle() {
@@ -21,8 +25,13 @@ public class SeparatistBaseEntrance implements Situation {
             return "You approach the separatist base where the separatists live. You want to explore, but a person " +
                     "comes up to you and initiates a conversation.";
         } else {
-            // if not enough reputation, chief tells you to still go away
-            return "temp";
+            if (chief.getHappiness() == 100) {
+                //new situation unlock
+                return "";
+            } else {
+                AdvGame.updateFrame(new String[]{"Leave"});
+                return "You are still not wanted. You haven't completed the quest yet.";
+            }
         }
     }
 
@@ -45,16 +54,24 @@ public class SeparatistBaseEntrance implements Situation {
                     AdvGame.updateFrame("The person introduces himself as the chief of the separatist village. Using his local power, he " +
                                     "tells you that you are not wanted, unless you can do him a favor.",
                             new String[]{"Talk to the Chief","Leave"});
-                } else {
-                    // if reputation is not enough
-                    AdvGame.updateFrame("You are still not wanted",
-                            new String[]{"Talk to the Chief","Leave"});
                 }
+                break;
+
+            case "Talk to the Chief":
+                    AdvGame.updateFrame("The Chief tells you that his daughter is very sick. To cure her, she needs the holy liquid: Sparking Water. " +
+                                    "To cure her, you need to find three cans of sparkling water.",
+                            new String[]{"Ask Chief for information", "Leave"});
+                    break;
+
+            case "Ask Chief for information":
+                AdvGame.updateFrame("The Separatist have a casino that has never been won before. " +
+                                "To get three bottles of water, you need to win the slots machine.",
+                        new String[]{"Leave"});
+                firstTime = false;
                 break;
 
             // EXIT POINT
             case "Leave":
-                firstTime = false;
                 AdvGame.setSituation(SeparatistHub.getInstance());
                 break;
         }
