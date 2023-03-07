e07c9a14005e8d2c51da7eba8b73ff8bc649d73d
Tyler <tylerkn23@sherwoodsd.com>
2023-03-07 13:11:40 -0800

    work

---
+++ src/main/java/org/sherwoodhs/situation/Separatist/Money.java
@@ -0,0 +1,17 @@
+package org.sherwoodhs.situation.Separatist;
+
+public class Money {
+    private int money;
+
+    public Money(int money) {
+        this.money = money;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+}
