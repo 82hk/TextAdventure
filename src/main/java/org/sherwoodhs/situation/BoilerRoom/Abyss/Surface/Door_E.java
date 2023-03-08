6b4abc016ea8182cfd3d3317c4be2edfe18df93a
hangilk <hangilxk24@sherwoodsd.com>
2023-03-08 11:57:51 -0800

    Some changes 2

--- src/main/java/org/sherwoodhs/situation/BoilerRoom/Abyss/Surface/Door_E.java
+++ src/main/java/org/sherwoodhs/situation/BoilerRoom/Abyss/Surface/Door_E.java
@@ -59,7 +59,7 @@ public class Door_E implements Situation {
                 World.changeStateI("Renown", -1);
                 doorIsOpen = true;
                 AdvGame.clearFrameWithoutSpacing(
-                        "You open the door, and it creaks. You can see only darkness inside.",
+                        "You open the door, and it creaks. You can see only darkness inside. You can sense that you're losing something by wasting your time here...",
                         new String[]{"Close the door", "Enter", "Return to the outcropping"}
                 );
                 break;
