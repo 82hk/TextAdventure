c63c68e71f6c0c8e584fd66be85cae742daa7c4f
Chris <christopherip23@sherwoodsd.com>
2023-04-19 10:31:07 -0700

    Same image for all situations

--- src/main/java/org/sherwoodhs/ui/MapPanel.java
+++ src/main/java/org/sherwoodhs/ui/MapPanel.java
@@ -9,7 +9,6 @@ public class MapPanel extends JPanel {
     public static final MapPanel mapPanel = new MapPanel();
 
     ImageIcon placeholder = new ImageIcon("src/main/java/org/sherwoodhs/imgs/placeholder.png");
-
     ImageIcon img = new ImageIcon("src/main/java/org/sherwoodhs/imgs/entrance/entrance.jpg");
     ImageIcon scaledImg = new ImageIcon(img.getImage().getScaledInstance(240, 240, Image.SCALE_DEFAULT));
     JLabel label = new JLabel(scaledImg, JLabel.CENTER);
@@ -18,13 +17,17 @@ public class MapPanel extends JPanel {
         super();
         setPreferredSize(new Dimension(250, 250));
         //setBorder(new TitledBorder("Map"));
+        img.setImage(img.getImage().getScaledInstance(240,240,Image.SCALE_DEFAULT));
 
         add(label);
     }
 
     //use to update the image, there are a gazillion updateframe methods and im gonna figure out how which ones to put it in
     public void updateImg(){
-        try {
+
+        label.setIcon(img);
+
+        /* try {
             img = AdvGame.getInstance().getCurrentSituation().getImg();
             scaledImg = new ImageIcon(img.getImage().getScaledInstance(240, 240, Image.SCALE_DEFAULT));
 
@@ -32,6 +35,6 @@ public class MapPanel extends JPanel {
             label.setIcon(scaledImg);
         } catch (NullPointerException e){
             label.setIcon(placeholder);
-        }
+        } */
     }
 }
